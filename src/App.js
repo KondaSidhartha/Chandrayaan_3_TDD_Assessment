5c5af6b4c932c57032fdc7d33e6def6420234687
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2023-08-02 14:38:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-// Test Case (compilation will fail):
-test('Executing the "f" command updates the position correctly', () => {
-  const { getByText, getByLabelText } = render(<Chandrayaan3 />);
+import React, { useState } from "react";
 
-  fireEvent.change(getByLabelText("Enter Command:"), {
-    target: { value: "f" },
-  });
-  fireEvent.click(getByText("Execute Command"));
+const Chandrayaan3 = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+  const [direction, setDirection] = useState("N");
+  const [commandInput, setCommandInput] = useState("");
 
-  expect(getByText("Current Position: (0, 1, 0)")).toBeInTheDocument();
-  expect(getByText("Current Direction: N")).toBeInTheDocument();
-});
+  const executeCommand = (command) => {
+    switch (command) {
+      case "f":
+        setY(y + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleExecute = () => {
+    // Execute the command
+    executeCommand(commandInput);
+  };
+
+  return (
+    <div>
+      <h2>Chandrayaan 3</h2>
+      <p>
+        Current Position: ({x}, {y}, {z})
+      </p>
+      <p>Current Direction: {direction}</p>
+      <label>
+        Enter Command:
+        <input
+          type="text"
+          value={commandInput}
+          onChange={(e) => setCommandInput(e.target.value)}
+        />
+      </label>
+      <button onClick={handleExecute}>Execute Command</button>
+    </div>
+  );
+};
+
+export default Chandrayaan3;
