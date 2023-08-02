d40d3d912d6cef18e37c83c0fafa038cc622f199
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2023-08-02 14:40:10 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,72 @@ const Chandrayaan3 = () => {
   const executeCommand = (command) => {
     switch (command) {
       case "f":
-        setY(y + 1);
+        moveForward();
+        break;
+      case "b":
+        moveBackward();
+        break;
+      case "r":
+        turnRight();
+        break;
+      case "l":
+        turnLeft();
+        break;
+      case "u":
+        turnUp();
+        break;
+      case "d":
+        turnDown();
         break;
       default:
         break;
     }
+  };
+
+  const moveForward = () => {
+    if (direction === "N") setY(y + 1);
+    else if (direction === "S") setY(y - 1);
+    else if (direction === "E") setX(x + 1);
+    else if (direction === "W") setX(x - 1);
+    else if (direction === "Up") setZ(z + 1);
+    else if (direction === "Down") setZ(z - 1);
+  };
+
+  const moveBackward = () => {
+    if (direction === "N") setY(y - 1);
+    else if (direction === "S") setY(y + 1);
+    else if (direction === "E") setX(x - 1);
+    else if (direction === "W") setX(x + 1);
+    else if (direction === "Up") setZ(z - 1);
+    else if (direction === "Down") setZ(z + 1);
+  };
+
+  const turnRight = () => {
+    if (direction === "N") setDirection("E");
+    else if (direction === "E") setDirection("S");
+    else if (direction === "S") setDirection("W");
+    else if (direction === "W") setDirection("N");
+  };
+
+  const turnLeft = () => {
+    if (direction === "N") setDirection("W");
+    else if (direction === "W") setDirection("S");
+    else if (direction === "S") setDirection("E");
+    else if (direction === "E") setDirection("N");
+  };
+
+  const turnUp = () => {
+    if (direction === "Up") setDirection("N");
+    else if (direction === "N") setDirection("Up");
+    else if (direction === "Down") setDirection("S");
+    else if (direction === "S") setDirection("Down");
+  };
+
+  const turnDown = () => {
+    if (direction === "Down") setDirection("N");
+    else if (direction === "N") setDirection("Down");
+    else if (direction === "Up") setDirection("S");
+    else if (direction === "S") setDirection("Up");
   };
 
   const handleExecute = () => {
